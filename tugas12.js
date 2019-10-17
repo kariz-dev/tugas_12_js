a9625396599a2eb672eb660035e7bf2f31cb221c
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-18 01:37:59 +0700

    tugas 12 javascript

---
+++ tugas12.js
@@ -0,0 +1,17 @@
+/* Tugas 12 Javascript */
+// Muhammad Rizwan
+
+function tugasDuaBelas(){
+    var buah = ['pisang','jeruk','apel','mangga'];
+    console.log(buah); // menampilkan semua isi array
+    
+    buah.pop();
+    console.log(buah); // menampilkan isi array setelah di pop
+
+    buah.shift();
+
+    return buah; // menampilkan isi array setelah di shift
+}
+
+
+console.log(tugasDuaBelas());
